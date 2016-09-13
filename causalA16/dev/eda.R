6e3bca92f7ad4c783f9004321ddaba7e667e7353
lgan248 <gan.67@buckeyemail.osu.edu>
2016-09-13 12:18:19 -0400

    edited eda file

--- causalA16/dev/eda.R
+++ causalA16/dev/eda.R
@@ -1,4 +1,13 @@
+library(causalA16)
 library(data.table)
 library(ggplot2)
-install.packages("texreg")
 library(texreg)
+
+#open Karreth and Tir JOP article
+library(foreign)
+IOCW <- read.dta("/Users/liwugan/GitHub/Quant3_A16/causalA16/inst/extdata /igo.cw.replication.dta")
+
+
+dat <- load_dataset("igo.cw.replication.dta")
+setDT(dat)
+setkey(dat, onset)
